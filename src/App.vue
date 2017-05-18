a94af1c4a250556db0d0154e5e92bb483a9ad935
anC8899 <shenzongwen@gmail.com>
2017-05-18 17:25:18 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <!--<img src="./assets/logo.png">-->
+<link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!--<hello></hello>-->
+    <todos :todos="todos"></todos>
+    <todo-form :todos="todos"></todo-form>
+  </div>
+</template>
+
+<script>
+//import Hello from './components/Hello'
+import Todos from './components/Todos.vue'
+import TodoForm from './components/TodoForm.vue'
+
+export default {
+  name: 'app',
+  data(){
+    return {
+      todos: [
+        {id: 1, title: 'Learn Vuejs', completed: true}
+      ],
+    }
+  },
+  components: {
+    Todos,TodoForm
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TodoForm.vue
@@ -0,0 +1,31 @@
+<template>
+    <form v-on:submit.prevent="addTodo(newTodo)">
+        <div class="form-group">
+            <input type="text"
+                   class="form-control"
+                   v-model="newTodo.title"
+            />
+        </div>
+        <div class="form-group">
+            <button class="btn btn-success" type="submit"> Add Todo </button>
+        </div>
+    </form>
+</template>
+
+<script type="text/ecmascript-6">
+
+    export default{
+      props: ['todos'],
+      data(){
+        return {
+          newTodo: {id: null, title: '', completed: false}
+        }
+      },
+      methods: {
+        addTodo(newTodo){
+          this.todos.push(newTodo);
+          this.newTodo = {id: null, title: '', completed: false}
+        }
+      }
+    }
+</script>
